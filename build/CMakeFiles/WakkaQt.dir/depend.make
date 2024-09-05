c04b11008c98bcd441f937e413fdfc0b88d29b02
Gustavo L Conte <40776097+guprobr@users.noreply.github.com>
2024-09-05 00:15:47 -0300

    Delete build directory

--- build/CMakeFiles/WakkaQt.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for WakkaQt.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/WakkaQt.dir/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/WakkaQt.dir/qrc_resources.cpp.o: \
- /home/guzpido/WakkaQt/build/qrc_resources.cpp /usr/include/stdc-predef.h

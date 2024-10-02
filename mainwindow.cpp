43d198e99c9cd03e9a9a1da0adb63dfc22bbb125
Gustavo L Conte <suporte@gu.pro.br>
2024-10-01 22:02:44 -0300

    some windows machines has playback seeking BACKWARDS broken

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -927,12 +927,13 @@ bool MainWindow::eventFilter(QObject *object, QEvent *event) {
                 if ( player ) {
                     qint64 newPosition = static_cast<qint64>(progress * player->duration());
 
-                    player->pause();                    // pause for smooth seeking
                     player->setAudioOutput(nullptr);    // avoid breaking sound when seeking (Qt6.4 bug?)
-                    player->setVideoOutput(nullptr);    // avoids breaking seeking on some Windows machines
+
                     player->setPosition(newPosition);  // Seek the media player to the clicked position
+                    player->pause();                    // pause for smooth seeking
+
                     player->setAudioOutput(audioOutput.data());
-                    player->setVideoOutput(videoWidget);
+
                     player->play();
 
                     return true;  // Event handled

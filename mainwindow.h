958abd535055a941485a36a6ee53fc27f93788ef
Gustavo L Conte <suporte@gu.pro.br>
2024-10-02 22:49:19 -0300

    v0.77alpha

--- mainwindow.h
+++ mainwindow.h
@@ -34,7 +34,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    QString Wakka_versione = "v0.69a";
+    QString Wakka_versione = "v0.77a";
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 

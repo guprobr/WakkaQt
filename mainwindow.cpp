7c572758bf9619454f91a828c173178fe20e1c47
Gustavo L Conte <suporte@gu.pro.br>
2024-10-12 18:15:01 -0300

    trying audio sync without OFFSET

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -582,7 +582,6 @@ void MainWindow::onPlayerMediaStatusChanged(QMediaPlayer::MediaStatus status) {
         // Start listening for the first duration change
             connect(mediaRecorder.data(), &QMediaRecorder::durationChanged, this, &MainWindow::onDurationChanged);
             mediaRecorder->record(); // start recording but wait recording state to start playback
-            audioRecorder->startRecording(audioRecorded);
         
         } else {
             vizPlayer->play(); // if not recording. Just play right after load
@@ -594,8 +593,11 @@ void MainWindow::onPlayerMediaStatusChanged(QMediaPlayer::MediaStatus status) {
 
 void MainWindow::onDurationChanged(qint64 currentDuration) {
 
-    if ( player->playbackState() == QMediaPlayer::PlaybackState::PlayingState )
+    if ( player->playbackState() == QMediaPlayer::PlaybackState::PlayingState ) {
         offset = currentDuration - player->position();
+        audioRecorder->startRecording(audioRecorded); // start audio recording with offset = 0
+    }
+        
     
 }
 
@@ -608,7 +610,7 @@ void MainWindow::onRecorderStateChanged(QMediaRecorder::RecorderState state) {
         singButton->setText("Finish!");
         singButton->setEnabled(true);
 
-        vizPlayer->play();
+        vizPlayer->play(); // now we are ready for playback
 
     }
 
@@ -939,7 +941,7 @@ void MainWindow::mixAndRender(double vocalVolume) {
                         afftdn=nf=-20:nr=10:nt=w,speechnorm,acompressor=threshold=0.5:ratio=4,highpass=f=200,%2 \
                         aecho=0.6:0.4:69|51:0.21|0.13,treble=g=12,volume=%3[vocals]; \
                         [2:a][vocals]amix=inputs=2:normalize=0,aresample=async=1[wakkamix];%4" 
-                        ).arg(offset)
+                        ).arg(0)
                         .arg(talent)
                         .arg(vocalVolume)
                         .arg(videorama);

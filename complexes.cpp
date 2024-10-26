cc996ae45a5a3b5428b140455346edd3842b0d53
Gustavo L Conte <suporte@gu.pro.br>
2024-10-26 19:04:40 -0300

    remove treble filter and do use pitchShiftRatio!

--- complexes.cpp
+++ complexes.cpp
@@ -1,4 +1,4 @@
 #include "complexes.h"
 
 // FFMpeg filter_complexes
-    QString _audioMasterization = "aformat=channel_layouts=stereo,afftdn,deesser,speechnorm,acompressor=threshold=0.5:ratio=4,highpass=f=200,aecho=0.9:0.9:84|111:0.21|0.13,treble=g=1,";
+    QString _audioMasterization = "aformat=channel_layouts=stereo,afftdn,deesser,speechnorm,acompressor=threshold=0.5:ratio=4,highpass=f=200,aecho=0.6:0.4:84|111:0.21|0.13,";

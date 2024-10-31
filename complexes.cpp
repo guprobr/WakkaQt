b87e9c9d490bc34593860b85630e1f6bb4c426aa
Gustavo L Conte <suporte@gu.pro.br>
2024-10-31 09:55:51 -0300

    adj treble but with deesser filter

--- complexes.cpp
+++ complexes.cpp
@@ -6,7 +6,7 @@
 // FFMpeg filter_complexes
     QString _audioMasterization = "aformat=channel_layouts=stereo,afftdn,deesser,speechnorm,acompressor=threshold=0.5:ratio=4,highpass=f=200,";
     QString _filterEcho = "aecho=0.8:0.7:32|64:0.21|0.13,";
-    QString _filterTreble = "treble=g=8,";
+    QString _filterTreble = "treble=g=12,deesser,";
 
 // fixed tmp file paths
     QString webcamRecorded = QDir::temp().filePath("WakkaQt_tmp_recording.mp4");

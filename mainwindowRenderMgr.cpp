eb5367ec68a1093d1015425c1213050d9cb3a948
Gustavo L Conte <suporte@gu.pro.br>
2025-02-19 16:59:09 -0300

    Discovered nasty bug on some video filters at final pipeline!

--- mainwindowRenderMgr.cpp
+++ mainwindowRenderMgr.cpp
@@ -113,7 +113,7 @@ void MainWindow::mixAndRender(double vocalVolume, qint64 manualOffset) {
     QString offsetFilter;
 
     if (videoOffset + manualOffset < 0) {
-        offsetFilter = QString("delay=delay=%1|%1").arg(-videoOffset + manualOffset);
+        offsetFilter = QString("tpad=start_duration=%1").arg((-videoOffset + manualOffset)/1000);
     } else {
         offsetFilter = QString("trim=%1ms").arg(videoOffset + manualOffset);
     }

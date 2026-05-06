d9fbb9260570de7c62978e04f417b7978d20f374
Gustavo L Conte <suporte@gu.pro.br>
2026-05-06 01:26:06 -0300

    prevent more than one instance running at same time

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDir>
 #include <QIcon>
+#include <QLockFile>
+#include <QMessageBox>
 #include <QPalette>
 #include <QStyleFactory>
 #include <QStyleHints>
@@ -20,7 +23,18 @@ int main(int argc, char *argv[]) {
     QApplication WakkaQt(argc, argv);
 
     WakkaQt.setWindowIcon(QIcon(":/images/icon.png"));
-  
+
+    QLockFile lockFile(QDir::tempPath() + "/WakkaQt.lock");
+    if (!lockFile.tryLock(100)) {
+        QMessageBox::warning(
+            nullptr,
+            "WakkaQt is already running",
+            "Another instance of WakkaQt is already running.\n\n"
+            "Please close or kill the existing WakkaQt process before starting a new one."
+        );
+        return 1;
+    }
+
     MainWindow w;
     w.show();
     return WakkaQt.exec();
